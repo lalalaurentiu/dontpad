802682cc8a28c7ab459976865e1449ebec109b23
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2022-10-14 20:11:45 +0300

    create get dontpadurl if exist else create

--- dontpadcode/models.py
+++ dontpadcode/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 #Modelul pentru noul proiect
 class DontpadURL(models.Model):
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
 
+    def __str__(self):
+        return self.slug
+        
 #Modelul pentru code
 class DontpadCode(models.Model):
     slug = models.ForeignKey(DontpadURL, on_delete = models.CASCADE, related_name = "url")
--- dontpadcode/views.py
+++ dontpadcode/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
 def new_file(request, slug):
+    
     template_name = "file.html"
+
+    file_name = request.path.replace("/", "")
+
+    obj = DontpadURL.objects.get_or_create(slug=file_name)
+    print(obj[0].id)
 
     response = render(request, template_name)
     return response
