586fff6988d739ad63103ffe68a7f4ad05e688d2
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2022-10-14 18:17:11 +0300

    create dontpadcode app models

--- dontpadcode/models.py
+++ dontpadcode/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+#Modelul pentru noul proiect
+class DontpadURL(models.Model):
+    slug = models.SlugField()
+
+#Modelul pentru code
+class DontpadCode(models.Model):
+    slug = models.ForeignKey(DontpadURL, on_delete = models.CASCADE, related_name = "url")
+    code = models.TextField()
