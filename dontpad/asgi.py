6e48c0c459cb047c218e091ef7ebe0ebd8714cbb
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2022-10-22 19:16:50 +0300

    rollback

--- dontpad/asgi.py
+++ dontpad/asgi.py
@@ -12,15 +12,13 @@ import os, django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+from dontpadcode.routing import websocket_urlpatterns
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dontpad.settings')
 django.setup()
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from dontpadcode.routing import websocket_urlpatterns
+
 
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
