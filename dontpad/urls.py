a8ca4d739007baf8cdfbe889daf31049164cc5b0
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2022-10-22 18:07:52 +0300

    adding statics in  urls.py

--- dontpad/urls.py
+++ dontpad/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("<str:slug>", include("dontpadcode.urls", namespace="code"))
-]
+    path("<str:slug>", include("dontpadcode.urls", namespace="code")),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}, name='static'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
