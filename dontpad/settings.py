054d89e1efc0b5ee622e47a207cdfc168ef00615
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2022-10-23 02:24:31 +0300

    retry

--- dontpad/settings.py
+++ dontpad/settings.py
@@ -78,10 +78,10 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get('REDIS_URL')],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
-    'ROUTING': 'dontpad.routing.application',
+    'ROUTING': 'dontpadcode.routing.application',
 }
 
 
