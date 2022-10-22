310b31cdda589504fb82a10442b9970add689438
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2022-10-22 23:53:26 +0300

    change asgi file

--- dontpad/asgi.py
+++ dontpad/asgi.py
@@ -19,16 +19,21 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from dontpadcode.routing import websocket_urlpatterns
+from channels.security.websocket import AllowedHostsOriginValidator
 
 from channels.layers import get_channel_layer
 
 channel_layer = get_channel_layer()
 
+django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
-  'http': get_asgi_application(),
-  'websocket': AuthMiddlewareStack(
-    URLRouter(
-      websocket_urlpatterns
-    ),
+  'http': django_asgi_app,
+  'websocket': AllowedHostsOriginValidator(
+      AuthMiddlewareStack(
+      URLRouter(
+        websocket_urlpatterns
+      ),
+    )
   )
+  
 })
