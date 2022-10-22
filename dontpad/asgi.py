bd4662f4b1ebd425ff8018d3dfb643d5bdb37c49
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2022-10-22 20:35:37 +0300

    retry push on heroku

--- dontpad/asgi.py
+++ dontpad/asgi.py
@@ -9,20 +9,22 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os, django
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 
-from dontpadcode.routing import websocket_urlpatterns
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dontpad.settings')
 django.setup()
 
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from dontpadcode.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
-  'websocket': URLRouter(
+  'websocket': AuthMiddlewareStack(
+    URLRouter(
       websocket_urlpatterns
     ),
+  )
 })
