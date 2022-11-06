f9dc6aedb4bb250aa344ecbf540b172884c5753f
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2022-11-06 10:22:36 +0200

    adding login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import *
-
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register
 app_name = "accounts"
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name = "accounts/login.html"), name='login'),
 ]
