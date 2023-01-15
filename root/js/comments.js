409e3dd394d249658a3e30a651650a95ec5ff4cc
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-01-16 01:03:41 +0200

    show users in interactive comments

--- root/js/comments.js
+++ root/js/comments.js
@@ -28,12 +28,29 @@ versions.forEach(function(element){
 
         comments.then((result) => {
             obj = result;
+            console.log(obj);
             let objectCommentsContainer = {};
             obj.comments.forEach(function(comment) {
                 if (objectCommentsContainer[comment.line] != undefined){
-                    objectCommentsContainer[comment.line] += comment.comment + "<br>";
+                    objectCommentsContainer[comment.line] += `
+                        <div class="comment">
+                            <span class="commentAuthor">${comment.user.first_name} ${comment.user.last_name}</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                            </svg>
+                            <span>${comment.comment}</span>
+                        </div>
+                    `;
                 }else{
-                    objectCommentsContainer[comment.line] = comment.comment + "<br>";
+                    objectCommentsContainer[comment.line] = `
+                        <div class="comment">
+                            <span class="commentAuthor">${comment.user.first_name} ${comment.user.last_name}</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                            </svg>
+                            <span>${comment.comment}</span>
+                        </div> 
+                    `;
                 }
                 
             });
@@ -110,9 +127,25 @@ versions.forEach(function(element){
                 let objectCommentsContainer = {};
                 obj.comments.forEach(function(comment) {
                     if (objectCommentsContainer[comment.line] != undefined){
-                        objectCommentsContainer[comment.line] += comment.comment + "<br>";
+                        objectCommentsContainer[comment.line] += `
+                            <div class="comment">
+                                <span class="commentAuthor">${comment.user.first_name} ${comment.user.last_name}</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                                </svg>
+                                <span>${comment.comment}</span>
+                            </div> 
+                        `;
                     }else{
-                        objectCommentsContainer[comment.line] = comment.comment + "<br>";
+                        objectCommentsContainer[comment.line] = `
+                            <div class="comment">
+                                <span class="commentAuthor">${comment.user.first_name} ${comment.user.last_name}</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                                </svg>
+                                <span>${comment.comment}</span>
+                            </div> 
+                        `;
                     }
                     
                 });
